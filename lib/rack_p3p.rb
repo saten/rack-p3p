f2b5b1115392361c9cf815aeefd4af8b07c61ef5
Sante Rotondi <sante.rotondi@dsmart.it>
2012-04-04 11:42:00 +0200

    policy swap

--- lib/rack_p3p.rb
+++ lib/rack_p3p.rb
@@ -26,7 +26,8 @@ module Rack
 
     # returns a default policy stating that no contact information will be stored
     def self.default_policy
-      %q{NOI ADM DEV PSAi COM NAV OUR OTRo STP IND DEM}
+       "ALL DSP COR PSAa PSDa OUR NOR ONL UNI COM NAV"
+#      %q{NOI ADM DEV PSAi COM NAV OUR OTRo STP IND DEM}
     end
 
     private
